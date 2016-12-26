262fe568712d7f6e6a7c12835754f123c6591f09
Michal Nazarewicz <mina86@mina86.com>
2016-12-26 17:16:39 +0100

    Add license

--- src/make-float.h
+++ src/make-float.h
@@ -1,3 +1,22 @@
+/*
+ * Implementation of some integer→real conversion methods.
+ * Copyright © 2016 by Michał Nazarewicz <mina86@mina86.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License.  You may obtain a copy
+ * of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+/* See https://mina86.com/2016/random-reals/ for discussion. */
+
 #include <stdint.h>
 #include <string.h>
 #include <float.h>
